686e4191102bbfae90225a49ebd8b2350fce97e3
amplil <aikotobahaaburakatabura@gmail.com>
2021-09-13 00:20:51 +0900

    コピー機能

--- price_comparison_cart.js
+++ price_comparison_cart.js
@@ -8,6 +8,9 @@ const app=new Vue({
       cartItems:[],
       loading: true,
       errored: false,
+      pop_disabled: true,
+      pop_nofade: true,
+      pop_show: false,
       }
   },
   mounted() {
@@ -36,68 +39,60 @@ const app=new Vue({
       .catch(error => {
         console.log(error);
       })
-    }
+    },
+    copy() {
+      let copy_text="";
+      for(let id in this.cartItems){
+        copy_text=copy_text
+        .concat(this.cartItems[id].title)
+        .concat(": ")
+        .concat(this.cartItems[id].quantity)
+        .concat("個\n");
+      }
+      //console.log(copy_text)
+      //this.$copyText(this.$el.querySelector("#items").textContent).then(
+      this.$copyText(copy_text).then(
+          (e) => {
+          //console.log("テキストコピー完了");
+          //console.log(e);
+
+          this.$refs.popover.$emit("enable");
+          this.pop_show = true;
+          setTimeout(() => {
+            this.pop_show = false;
+            this.$refs.popover.$emit("disable");
+          }, 1000);
+        },
+        (e) => {
+          console.log("コピーエラー");
+          console.error(e);
+        }
+      );
+    },
+    cart_modify(id){
+      // 引数が何もないときphpのsessionのカート情報を呼び出すだけの機能になる
+      axios.post('add_cart.php',{
+        item:{
+          item_id:id,
+          image:this.cartItems[id].image,
+          url:this.cartItems[id].url,
+          title:this.cartItems[id].title,
+          price:this.cartItems[id].price,
+          shop:this.cartItems[id].shop,
+          quantity:this.cartItems[id].quantity,
+        }
+      })
+      .then(response => {
+        this.cartItems = response.data
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    },
   }
 })
+
 /*
-$.post({
-  url: 'add_cart.php',
-  data:add_data={}, // 引数が何もないときphpのsessionのカート情報を呼び出すだけの機能になる
-  dataType: 'json' //必須。json形式で返すように設定
-}).done(function(data){
-  let cart=data;
-  let items_ele=document.getElementById('items');
-  //console.log(cart);
-  //console.log(item_data);
-
-  let disp_str='';
-  for(let id in cart) {
-    disp_str=disp_str.concat(
-      '<tr><td>'+cart[id].title+'</td>'
-      +'<td>'+cart[id].num+'個</td>'
-      +'<td><select id='+id+'>');
-    for (let i = 1; i < 10; i++){
-      disp_str=disp_str.concat('<option value="'+i+'">'+i+'</option>');
-    } // 個数選択
-    disp_str=disp_str.concat(
-      '</select></td>'
-      +'<td><button type="button" class="btn add_goods">削除</button></td>'
-      +'</tr>');
-  }
-  items_ele.insertAdjacentHTML('beforeend',disp_str);
-}).fail(function(XMLHttpRequest, textStatus, errorThrown){
-    alert(errorThrown);
-})  
-*/
-/*
-let cart_open = function(){
-  $("#goods_list").html('');
-  
-  let html = '';
-  let key;
-  let total = 0;
-  html += '<div>';
-  for (key in goods){
-    html += '<div class="item col-xs-3">';
-    html += '<div class="img-block"><img src="'+goods[key].image+'"></div>';
-    html += '<i class="far fa-trash-alt"></i></div>';
-    total += goods[key].sub_total;
-  }
-  html += '</div>';
-  html = '<div id="total">小計:'+comma( total )+'円</div>'+html;
-
-  //オブジェクトなのでそのままではPOSTの出来ないためJSON形式にする
-  //let data = JSON.stringify(goods);
-  
-  $("#goods_list").html(html); //上部カートにHTMLを挿入
-  $("#cart_detail").show();    //カートを開く
-  //$("#data").val(data);        //POST[data]にカートの中身をセット
-}
-
-function comma(num) { // 3桁ごとにカンマ
-    return num.toString().replace( /([0-9]+?)(?=(?:[0-9]{3})+$)/g , '$1,' );
-} 
-*/
 setTimeout(function () {
   $('#copy-button')
   // tooltip設定
@@ -133,3 +128,4 @@ setTimeout(function () {
       $(this).tooltip('show');
   });
 }, 545*1.33); // 545ms timing to load jQuery.js + network estimated delay
+*/
